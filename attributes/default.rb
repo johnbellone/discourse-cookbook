df7e0d795718b0e6c961aca0a8c03436a8e6f5db
John Bellone <john.bellone.jr@gmail.com>
2014-11-04 06:39:51 -0500

    Fix artifact location to deploy a tarball.
    The artifact cookbook deploys tarballs only and not git
    repositories. Perhaps this is something we can change in the upstream!

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,28 +1,14 @@
 #
 # Cookbook Name:: discourse
 # Recipe:: default
+# License:: Apache 2.0 (see http://www.apache.org/licenses/LICENSE-2.0)
 #
-# Copyright (C) 2014 John Bellone (<jbellone@bloomberg.net>)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-default['discourse']['artifact_location'] = "https://github.com/discourse/discourse/%{version}"
+default['discourse']['artifact_location'] = "https://github.com/discourse/discourse/tarball/%{version}"
 default['discourse']['artifact_version'] = '1.0.0'
 
 default['discourse']['site_name'] = 'meta.discourse.org'
 default['discourse']['site_root'] = "/srv/%{name}"
-default['discourse']['site_environment'] = {}
+default['discourse']['site_environment'] = { environment: 'production' }
 
 default['discourse']['user'] = 'discourse'
 default['discourse']['group'] = 'discourse'
